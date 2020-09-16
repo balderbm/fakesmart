37d11caddb6e4b1cdcf29fd611b93c9dea7c2e2c
Balder Bryn Morsund <balder.morsund@gmail.com>
2020-09-16 09:43:05 +0200

    liten while loop i main

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from DL import Map_Obj, astar
 
 def main():
-    #Specifies task number
-    task = 5
+    task = 1
+    while task != 5:
 
-    #Creates a map object
-    mapobj=Map_Obj(task)
+        #Creates a map object
+        mapobj=Map_Obj(task)
 
-    #Gets start and goal from map object
-    start=tuple(mapobj.start_pos)
-    end=tuple(mapobj.goal_pos)
+        #Gets start and goal from map object
+        start=tuple(mapobj.start_pos)
+        end=tuple(mapobj.goal_pos)
 
-    #Calles astar-algorithm
-    path, end = astar(mapobj, start, end, task)
+        #Calles astar-algorithm
+        path, end = astar(mapobj, start, end, task)
 
-    #Draws path on map
-    for p in path:
-        mapobj.replace_map_values(p,0,end)
-    mapobj.show_map()
-    print(path)
+        #Draws path on map
+        for p in path:
+            mapobj.replace_map_values(p,0,end)
+        mapobj.show_map()
+        print(path)
+
+        task = task + 1
+
+        #input("Press Enter to continue...")
 
 main()
